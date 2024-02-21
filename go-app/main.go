f5eedbe250b1dcdd12e2ec1e4028df833b91a8d4
ryo9723 <sti555amr@gmail.com>
2024-02-21 14:53:00 +0900

    add code

---
+++ go-app/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Appointment struct {
+	ID                    int    `json:"id"`
+	NextAppointmentDate   string `json:"nextAppointmentDate"`
+	ContractedSales       int    `json:"contractedSales"`
+	CurrentContractCount  int    `json:"currentContractCount"`
+	CompanyName           string `json:"companyName"`
+	CompanyNameKana       string `json:"companyNameKana"`
+	Capital               int    `json:"capital"`
+	EmployeesCount        int    `json:"employeesCount"`
+	AppointmentDepartment string `json:"appointmentDepartment"`
+	ContactPersonName     string `json:"contactPersonName"`
+	ContactPersonKana     string `json:"contactPersonKana"`
+	URL                   string `json:"url"`
+}
+
+func getAppointments(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "root:rootroot@/management_system")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM appointments")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var appointments []Appointment
+	for rows.Next() {
+		var a Appointment
+		if err := rows.Scan(
+			&a.ID,
+			&a.NextAppointmentDate,
+			&a.ContractedSales,
+			&a.CurrentContractCount,
+			&a.CompanyName,
+			&a.CompanyNameKana,
+			&a.Capital,
+			&a.EmployeesCount,
+			&a.AppointmentDepartment,
+			&a.ContactPersonName,
+			&a.ContactPersonKana,
+			&a.URL,
+		); err != nil {
+			log.Fatal(err)
+		}
+		appointments = append(appointments, a)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	json.NewEncoder(w).Encode(appointments)
+}
+
+func main() {
+	http.HandleFunc("/", getAppointments)
+	fmt.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
